cdd2b20959f7047c612e30c05b6e05490d011572
teckerz101 <isaacclifton@hotmail.com>
2024-06-14 11:54:00 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const daysLeftOut = document.getElementById('daysLeft')
+
+const timerEndDate = new Date("06-17-2024")
+
+window.onload = function() {
+    console.log('onload called')
+    setTimeRemaining();
+};
+
+setInterval(function(){ 
+    console.log('interval called')
+    setTimeRemaining();  
+}, 3600000);
+
+
+function setTimeRemaining() {
+    console.log('setTimeRemaining called');
+    console.log(timerEndDate);
+    let timeRemaining = timerEndDate.getTime() - new Date().getTime();
+    console.log(timeRemaining);
+    let daysRemaining = Math.round(timeRemaining / (1000 * 3600 * 24));
+    console.log(daysRemaining);
+    daysLeftOut.innerHTML = daysRemaining;
+};
